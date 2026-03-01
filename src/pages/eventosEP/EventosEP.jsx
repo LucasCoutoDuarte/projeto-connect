3261f089496193641b99a7a0e14400224fdeb740
LucasCoutoDuarte <lucascoutoduarte@gmail.com>
2026-03-01 12:53:49 -0300

    255

--- src/pages/eventosEP/EventosEP.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function EventosEP() {
-  return (
-    <div>
-      <h1>Eventos e Palestras</h1>
-    </div>
-  );
-}
--- src/pages/mentoria/Mentoria.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Mentoria() {
-  return (
-    <div>
-      <h1>Mentoria</h1>
-    </div>
-  );
-}
