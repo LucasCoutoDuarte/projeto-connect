5f30663002b19e6752bd9b59fcf7324256130d9a
LucasCoutoDuarte <lucascoutoduarte@gmail.com>
2026-03-01 12:57:56 -0300

    221

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
+import Footer from "../footer/Footer";
+import Header from "../header/Header";
 import l from "./Layout.module.scss";
 
 export default function Layout() {
